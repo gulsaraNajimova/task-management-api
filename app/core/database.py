09c6cb83bb89d1c500f3ce43abda348adad866dc
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-12-13 10:50:52 +0900

    Connect db through configs func

--- app/core/database.py
+++ app/core/database.py
@@ -3,10 +3,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
+from app.core.config import configs
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL) 
+engine = create_engine(configs.DATABASE_URI, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
