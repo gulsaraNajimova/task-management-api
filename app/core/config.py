46d66522ebfb536501cb7f6df3aaa836c7c2f2b4
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2024-05-12 23:08:32 +0900

    edit config settings for db engine

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 # Load environment variables from .env file
 load_dotenv()
@@ -10,12 +10,17 @@ class Configs(BaseSettings):
     # auth
     SECRET_KEY: str = os.getenv("MY_SECRET_KEY", "")
 
+    DB_ENGINE_MAPPER: dict = {
+        "postgresql": "postgresql",
+        "mysql": "mysql+pymysql",
+    }
+
     # database
-    DB: str = os.getenv("DB", "postgres")
+    DB: str = os.getenv("DB", "postgresql")
     DB_USER: str = os.getenv("DB_USER")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD")
     DB_HOST: str = os.getenv("DB_HOST")
-    DB_PORT: str = os.getenv("DB_PORT", 5432)
+    DB_PORT: str = os.getenv("DB_PORT")
     DB_ENGINE: str = "postgresql"
 
     DATABASE_URI_FORMAT: str = "{db_engine}://{user}:{password}@{host}:{port}/{database}"
